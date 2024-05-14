f6db57a660bb88a74a73396a2a1443e004f50591
Ar7 <aravinthssivakumar@outlook.com>
2024-05-14 10:08:50 +0530

    password updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.secret_key = "Shit"
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'Anjana@2005'
 app.config['MYSQL_DB'] = 'nookncorner'
 
 mysql = MySQL(app)
